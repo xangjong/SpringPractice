b50dac74cc8deac30b16e6093fe77c90a29781f2
xangjong <csj7080@naver.com>
2022-12-18 13:39:53 +0900

    object-mapper2

---
+++ FastCampus/object-mapper2/src/main/java/org/example/dto/Car.java
@@ -0,0 +1,47 @@
+package org.example.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+public class Car {
+
+    private String name;
+    @JsonProperty("car_number")
+    private String carNumber;
+    @JsonProperty("TYPE")
+    private String type;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCarNumber() {
+        return carNumber;
+    }
+
+    public void setCarNumber(String carNumber) {
+        this.carNumber = carNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", carNumber='" + carNumber + '\'' +
+                ", type='" + type + '\'' +
+                '}';
+    }
+}
