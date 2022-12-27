74a58b497696e2140110469d7b2e46f7724f0944
xangjong <csj7080@naver.com>
2022-12-27 23:36:49 +0900

    Add spring-calculator

---
+++ FastCampus/spring-calculator/src/main/java/com/example/springcalculator/component/MarketApi.java
@@ -0,0 +1,14 @@
+package com.example.springcalculator.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MarketApi {
+
+    private int price = 1;
+
+    public int connect(){
+        return 1100;
+    }
+
+}
---
+++ FastCampus/spring-calculator/src/main/java/com/example/springcalculator/dto/Req.java
@@ -0,0 +1,13 @@
+package com.example.springcalculator.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Req {
+    private int x;
+    private int y;
+}
